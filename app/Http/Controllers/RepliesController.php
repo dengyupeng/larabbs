e44f750ffdcf57cbea6a1b5407160db963b1ba5b
dengyupeng <dengyupeng@dengyupengdeMacBook-Pro.local>
2018-03-31 03:30:08 +0800

    Integrate laravel-permission package

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -33,7 +33,8 @@ class RepliesController extends Controller
 
     public function store(ReplyRequest $request, Reply $reply)
     {
-        $reply->content = $request->content;
+        // reply->content = $request->content;
+        $reply->content = $request->input('content');
         $reply->user_id = Auth::id();
         $reply->topic_id = $request->topic_id;
         $reply->save();
--- public/js/app.js
+++ public/js/app.js
@@ -43288,7 +43288,7 @@ if (false) {
 /* 42 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n.users-show-page {\n                 ^\n      Invalid CSS after \".users-show-page {\": expected \"}\", was \".\"\n      in /Users/dengyupeng/Code/larabbs/resources/assets/sass/app.scss (line 114, column 19)\n    at runLoaders (/Users/dengyupeng/Code/larabbs/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/dengyupeng/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/dengyupeng/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/dengyupeng/Code/larabbs/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/dengyupeng/Code/larabbs/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/Users/dengyupeng/Code/larabbs/node_modules/neo-async/async.js:7921:18)\n    at options.error (/Users/dengyupeng/Code/larabbs/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
