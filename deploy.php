ced88066b0a753c3074f94318c739c9ef4f1e214
dengyupeng <dengyupeng@dengyupengdeMacBook-Pro.local>
2018-03-30 19:52:56 +0800

    Basic user profile editing

--- deploy.php
+++ deploy.php
@@ -6,19 +6,19 @@ require 'recipe/laravel.php';
 // Project name
 set('application', 'larabbs');
 
-// Project name
-set('repository', 'git@gitee.com:dengyupeng/larabbs.git');
-
+// Project repository
+set('repository', 'https://github.com/dengyupeng/larabbs.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', true);
 
-// Shared files/dirs between deploys 
+// Shared files/dirs between deploys
 add('shared_files', []);
 add('shared_dirs', []);
 
-// Writable dirs by web server 
+// Writable dirs by web server
 add('writable_dirs', []);
+
 
 // Hosts
 
@@ -26,7 +26,7 @@ host('eiomi_test')
     ->stage('test')
     ->set('branch','dev')
     ->set('deploy_path', '/var/www/larabbs');
-    
+
 // Tasks
 
 task('build', function () {
