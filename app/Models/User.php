27b1eb0411b7e50d528a2efe09857ee8a4cdd9f3
dengyupeng <dengyupeng@dengyupengdeMacBook-Pro.local>
2018-03-31 05:59:34 +0800

    larabbs  完成

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,6 @@ class User extends Authenticatable
     use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
-    use Notifiable;
     use Notifiable {
         notify as protected laravelNotify;
     }
